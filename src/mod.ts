314738d88b53d377fe4813e615d72438e709b655
handoko <srabutdotcom@gmail.com>
2025-04-09 23:23:43 +0700

    remove uints.js from mod.ts as we made other version in @aicone/byte

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,5 @@
 export * from "./struct.js"
 export * from "./constrained.js"
 export * from "./utils.js"
-export * from "./uints.js"
 export * from "./hexstring.js"
 export * from "./safeuint8array.js"
